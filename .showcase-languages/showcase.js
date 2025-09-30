9adf9a25c9d7e2d8a2b60ab0a6dffd222a3fafce
Delyar21 <166810199+Delyar21@users.noreply.github.com>
2025-09-30 12:23:27 +0200

    Update showcase.js

--- .showcase-languages/showcase.js
+++ .showcase-languages/showcase.js
@@ -8866,3 +8866,5 @@
 // JavaScript is the main language of the project
 // Adding multiple lines so GitHub counts more JS
 // ...// Showcase file for GitHub language stats
+// Adding multiple lines so GitHub counts more JS
+// ...// Showcase file for GitHub language stats
